cf048b201f58e033ddd1d01ed5b8da8da3b78d9b
kaaamh96 <ahussainkamrul@hotmail.com>
2026-09-08 01:57:18 +0100

    tested and working

--- main.tf
+++ main.tf
@@ -1,4 +1,26 @@
 
+
+# following resources will be created
+#VPC
+# Subnets
+# Route table
+# Security groups
+# EC2 instances
+# User data
+# Target group
+# ALB
+# Listener
+
+
+
+
+
+
+
+
+
+
+
 resource "aws_vpc" "customvpc" {
 
   cidr_block = var.vpc_cidr
@@ -394,16 +416,16 @@ resource "aws_instance" "server_2" {
 #Target group 
 resource "aws_lb_target_group" "alb_target_group" {
 
-    name = "alb-target-group"
-    port = 80
-    protocol = "HTTP"
-    vpc_id = aws_vpc.customvpc.id
+  name     = "alb-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.customvpc.id
 
 
-    health_check {
-        path = "/"
-        protocol = "HTTP"
-        port                = "traffic-port"
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    port                = "traffic-port"
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 5
@@ -432,3 +454,38 @@ resource "aws_lb_target_group_attachment" "server_2_attachment" {
 
 
 #ALB
+
+resource "aws_lb" "application_load_balancer" {
+
+  name = "application-load-balancer"
+
+  internal = false
+
+  load_balancer_type = "application"
+
+  security_groups = [
+    aws_security_group.alb_sg.id
+  ]
+
+  subnets = [
+    aws_subnet.public_subnet_1.id,
+    aws_subnet.public_subnet_2.id
+
+  ]
+
+  tags = {
+    Name = "application-load-balancer"
+  }
+
+}
+
+resource "aws_lb_listener" "http_listerner" {
+  load_balancer_arn = aws_lb.application_load_balancer.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+  }
+}
